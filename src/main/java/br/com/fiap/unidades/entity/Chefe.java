061ed39de5b2fca4423fab49ee586e3767568724
ThiagoTodeschini <126583151+ThiagoTodeschini@users.noreply.github.com>
2024-04-17 10:46:40 -0300

    RmNome

--- src/main/java/br/com/fiap/unidades/entity/Chefe.java
+++ src/main/java/br/com/fiap/unidades/entity/Chefe.java
@@ -1,31 +1,42 @@
 package br.com.fiap.unidades.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
-
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "UK_chefe_usuario_unidade", columnNames = {"usuario_id", "unidade_id", "fim"})
+})
 public class Chefe {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private Boolean substituto;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "unidade_id", nullable = false)
     private Unidade unidade;
 
+    @Column(nullable = false)
     private LocalDateTime inicio;
 
+    @Column(nullable = false)
     private LocalDateTime fim;
 
 }
